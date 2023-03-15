6ffea68bf8425260869fc212544fa6d524b54761
b-jesch <birger.jesch@gmail.com>
2023-03-15 13:20:11 +0100

    improved Music OSD settings

--- scripts/toggle_addon_activation.py
+++ scripts/toggle_addon_activation.py
@@ -3,4 +3,3 @@ import sys
 
 jsonrpc = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": {"addonid": "%s", "enabled": %s}, "id": 1}' % (sys.argv[1], sys.argv[2])
 xbmc.executeJSONRPC(jsonrpc)
-sys.exit(0)
