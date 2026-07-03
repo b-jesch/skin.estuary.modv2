c2d5bca4ccee46a32e8180a685b49f6aacac22f0
b-jesch <birger.jesch@gmail.com>
2026-07-03 18:28:27 +0200

    Player Process Info updated pt. 2

--- scripts/services.py
+++ scripts/services.py
@@ -64,12 +64,10 @@ if __name__ == '__main__':
     while not monitor.abortRequested():
         if monitor.waitForAbort(2): break
 
-        # get average CPU usage over all CPU cores
+        # iterates CPU load over all available cores and set the average value as property 'avgCPU'
 
-        cores = re.findall('(\d+)%', xbmc.getInfoLabel('System.CpuUsage'))
-        sum = 0
-        for core in cores: sum += int(core)
-        if len(cores) > 0: xbmcgui.Window(10000).setProperty('avgCPU', '{}%'.format(sum // len(cores)))
+        cores = [int(core) for core in re.findall('(\d+)%', xbmc.getInfoLabel('System.CpuUsage'))]
+        if len(cores) > 0: xbmcgui.Window(10000).setProperty('avgCPU', '{}%'.format(sum(cores) // len(cores)))
 
         # call service viewswitcher
 
